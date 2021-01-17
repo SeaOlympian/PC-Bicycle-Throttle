72b7fc0f52ab2fa808fba383050312ddb290613b
SeaOlympian <hesketh.aaron@gmail.com>
2021-01-16 21:17:09 -0500

    Added Debug Options
    Added a debug variable to control serial monitor outputs.

--- Bike_Throttle/Bike_Throttle.ino
+++ Bike_Throttle/Bike_Throttle.ino
@@ -1,7 +1,8 @@
 //These variables can be changed based on user need and preference.
-int Hall_effect_pin = 14;   //Stores the pin number that the hall effect sensor is plugged into
-float maximumRPM = 90;  //Set to the maximum RPM that you want to pedal at for full throttle
-float minimumRPM = 50;  //Set to the RPM that will be the starting point for throttle input (NOTE: this input is the highest RPM that will be equal to ZERO throttle input)
+int Hall_effect_pin = 14;   //Stores the pin number that the hall effect sensor is plugged into.
+float maximumRPM = 90;  //Set to the maximum RPM that you want to pedal at for full throttle.
+float minimumRPM = 50;  //Set to the RPM that will be the starting point for throttle input (NOTE: this input is the highest RPM that will be equal to ZERO throttle input).
+int debug = 0;  //Outputs debung information to the serial monitor. Set to 0 for no debug options, 1 for hall effect sensor testing, 2 for RPM calculation testing, and 3 for throttle calculation testing.
 
 float RPMRange = maximumRPM - minimumRPM;
 
@@ -24,9 +25,6 @@ void count_function () {
 
 void rpm_calc () {
   delay(1000);
-
-  //Serial.println(count);
-
    
   //Turns off the interrupt command
   detachInterrupt(0);
@@ -40,8 +38,10 @@ void rpm_calc () {
   //Reset the count to prevent growing rpm reading
   count = 0;
 
-  Serial.print("RPM = ");
-  Serial.println(rpm);
+  if ((debug = 2)){
+    Serial.print("RPM = ");
+    Serial.println(rpm);
+  }
   
   attachInterrupt(digitalPinToInterrupt(Hall_effect_pin), count_function, RISING);
 }
@@ -52,8 +52,10 @@ void throttle_calc () {
   //Thanks to Wheatly_Inc, Wolfhaus, and FyronFD
   throttle = (rpm - minimumRPM)/RPMRange;
 
-  Serial.print("Throttle %: ");
-  Serial.println(throttle);
+  if ((debug = 3)){
+    Serial.print("Throttle %: ");
+    Serial.println(throttle);
+  }
 }
 
 //----------MAIN----------
@@ -62,8 +64,9 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(Hall_effect_pin, INPUT_PULLUP);
 
-  //Begin serial console
-  Serial.begin(9600);
+  if ((debug != 0)
+    //Begin serial console
+    Serial.begin(9600);
 
   //Sets up the interupt pin and triggered command
   attachInterrupt(digitalPinToInterrupt(Hall_effect_pin), count_function, RISING);
@@ -72,8 +75,9 @@ void setup() {
 //--------------------
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Serial.println(digitalRead(Hall_effect_pin));
+  if ((debug = 1))
+    Serial.println(digitalRead(Hall_effect_pin));
+    
   rpm_calc();
 
   throttle_calc();
